17a04e2f7886df01b9b2cc586bb8f19f4b39e54e
YAMADA-57 <k23057kk@aitech.ac.jp>
2024-11-28 15:45:48 +0900

    add Diary

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from diaries.DiarySample import DiarySample
 from diaries.kurokawaDiary import kurokawaDiary
+from diaries.SatoDiary import SatoDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
     DiarySample(),
     kurokawaDiary(),
+    SatoDiary()
 ] 
 
 for d in diaries:
