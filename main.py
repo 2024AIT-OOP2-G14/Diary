967b61681eed371bf8b41679d0387326edc996c6
Atama1126 <k23007kk@aitech.ac.jp>
2024-11-28 16:02:11 +0900

    mian.pyにインポートした

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from diaries.DiarySample import DiarySample
 from diaries.kurokawaDiary import kurokawaDiary
+from diaries.AtamaDiary import AtamaDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
     DiarySample(),
     kurokawaDiary(),
+    AtamaDiary(),
 ] 
 
 for d in diaries:
